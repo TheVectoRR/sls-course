5b9b508eef34bf41cec19b5c6d5ee69ac0fa5f38
Jonathan <jonathanoudshoorn@gmail.com>
2020-06-18 20:57:53 +0200

    new get all auctions end point

--- src/handlers/create-auction.js
+++ src/handlers/create-auction.js
@@ -1,7 +1,7 @@
 import { v4 as uuid } from 'uuid';
 import AWS from 'aws-sdk';
 import middy from '@middy/core';
-import jsonBodyParser from '@middy/http-json-body-parser';
+import httpJsonBodyParser from '@middy/http-json-body-parser';
 import httpEventNormalizer from '@middy/http-event-normalizer';
 import httpErrorHandler from '@middy/http-error-handler';
 import createError from 'http-errors';
@@ -36,7 +36,7 @@ async function createAuction(event, context) {
 }
 
 export const handler = middy(createAuction)
-    .use(jsonBodyParser())
+    .use(httpJsonBodyParser())
     .use(httpEventNormalizer())
     .use(httpErrorHandler());
 
