a002ad1d5bfc5e06e2d4cd7244e9771f7bfc0e61
Jonathan <jonathanoudshoorn@gmail.com>
2020-06-19 17:15:02 +0200

    closing auctions

--- src/handlers/process-auctions.js
+++ src/handlers/process-auctions.js
@@ -1,8 +1,17 @@
+import createError from 'http-errors';
 import { getEndedAuctions } from '../lib/get-ended-auctions';
+import { closeAuction } from '../lib/close-auction';
 
 async function processAuctions(event, context) {
-    const auctionsToClose = await getEndedAuctions();
-    console.log(auctionsToClose);
+    try {
+        const auctionsToClose = await getEndedAuctions();
+        const closePromises = auctionsToClose.map(auction => closeAuction(auction));
+        await Promise.all(closePromises);
+        return { closed: closePromises.length };
+    } catch (error) {
+        console.error(error);
+        throw new createError.InternalServerError(error);
+    }
 }
 
 export const handler = processAuctions;
