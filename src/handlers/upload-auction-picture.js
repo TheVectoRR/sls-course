57286df231f3072b904d10992a8c39172e8c578c
Jonathan <jonathanoudshoorn@gmail.com>
2020-06-29 11:45:17 +0200

    error handling

--- src/handlers/upload-auction-picture.js
+++ src/handlers/upload-auction-picture.js
@@ -1,5 +1,8 @@
 import { getAuctionById } from './get-auction';
 import { uploadPictureToS3 } from '../lib/upload-picture-to-s3';
+import middy from '@middy/core';
+import httpErrorHandler from '@middy/http-error-handler';
+import createError from 'http-errors';
 
 export async function uploadAuctionPicture(event) {
   const { id } = event.pathParameters;
@@ -7,8 +10,13 @@ export async function uploadAuctionPicture(event) {
   const base64 = event.body.replace(/^data:image\/\w+;base64,/,'');
   const buffer = Buffer.from(base64, 'base64');
 
-  const uploadToS3Result = await uploadPictureToS3(auction.id + '.jpg', buffer);
-  console.log(uploadToS3Result);
+  try {
+    const uploadToS3Result = await uploadPictureToS3(auction.id + '.jpg', buffer);
+    console.log(uploadToS3Result);
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
 
   return {
     statusCode: 200,
@@ -16,4 +24,5 @@ export async function uploadAuctionPicture(event) {
   };
 }
 
-export const handler = uploadAuctionPicture;
+export const handler = middy(uploadAuctionPicture)
+  .use(httpErrorHandler());
